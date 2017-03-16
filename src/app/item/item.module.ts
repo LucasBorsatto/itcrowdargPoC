49b8906c22bead87d34c0a74f1f86cfd1c66e335
LucasBorsatto <borsattolucas@gmail.com>
2017-03-16 01:49:57 -0300

    Added ng2-dnd drag and drop

--- src/app/item/item.module.ts
+++ src/app/item/item.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '@angular/material';
+import { DndModule } from 'ng2-dnd';
 
 import { ItemComponent } from './item.component';
 import { ItemService } from './item.service';
@@ -10,7 +11,8 @@ import { ItemService } from './item.service';
   imports: [
     CommonModule,
     FormsModule,
-    MaterialModule
+    MaterialModule,
+    DndModule.forRoot()
   ],
   exports: [
     ItemComponent
